fe1de97d7a70a4ee7ce1110ab33ed2eb37692f76
PoojaMangulkar <96859422+PoojaMangulkar@users.noreply.github.com>
2022-06-08 11:24:38 +0530

    Add files via upload

---
+++ SpringRootConfig.java
@@ -0,0 +1,11 @@
+package capp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class SpringRootConfig {
+	//TODO:Service , DAO, DataSOurse , Email sender or some other business layers beans
+	
+
+}
---
+++ SpringWebConfig.java
@@ -0,0 +1,11 @@
+package capp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class SpringWebConfig extends WebMvcConfigurerAdapter{
+
+}
